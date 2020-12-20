1f0cb0d2c23b69532d75228ec5484acf60fe4528
Archit Date <architdate@gmail.com>
2020-12-20 16:04:02 +0800

    basic cleanup

--- HomePlugin/BotController/PokeSysBotMini.cs
+++ HomePlugin/BotController/PokeSysBotMini.cs
@@ -5,67 +5,21 @@ namespace PKHeX.Core.Injection
     public class PokeSysBotMini
     {
         public readonly int BoxStart;
-        public readonly int SlotSize;
-        public readonly int SlotCount;
-        public readonly int GapSize;
-        public readonly LiveHeXVersion Version;
-        public readonly ICommunicator com;
-        public bool Connected => com.Connected;
+        public const int SlotSize = 344;
+        public const int SlotCount = 30;
+        public readonly int page;
+        public readonly SysBotMini sys;
 
-        public PokeSysBotMini(LiveHeXVersion lv, InjectorCommunicationType ict)
+        public PokeSysBotMini(int lv)
         {
-            Version = lv;
-            com = RamOffsets.GetCommunicator(lv, ict);
-            BoxStart = RamOffsets.GetB1S1Offset(lv);
-            SlotSize = RamOffsets.GetSlotSize(lv);
-            SlotCount = RamOffsets.GetSlotCount(lv);
-            GapSize = RamOffsets.GetGapSize(lv);
+            page = lv;
+            sys = new SysBotMini();
+            BoxStart = 0x1EC73028 + (30 * 32 * 344 * lv);
         }
 
-        private uint GetBoxOffset(int box) => (uint)BoxStart + (uint)((SlotSize + GapSize) * SlotCount * box);
-        private uint GetSlotOffset(int box, int slot) => GetBoxOffset(box) + (uint)((SlotSize + GapSize) * slot);
-
-        public byte[] ReadBox(int box, int len)
-        {
-            var bytes = com.ReadBytes(GetBoxOffset(box), len);
-            if (GapSize == 0)
-                return bytes;
-            var allpkm = new List<byte[]>();
-            var currofs = 0;
-            for (int i = 0; i < SlotCount; i++)
-            {
-                var StoredLength = SlotSize - 0x1C;
-                var stored = bytes.Slice(currofs, StoredLength);
-                var party = bytes.Slice(currofs + StoredLength + 0x70, 0x1C);
-                allpkm.Add(ArrayUtil.ConcatAll(stored, party));
-                currofs += SlotSize + GapSize;
-            }
-            return ArrayUtil.ConcatAll(allpkm.ToArray());
-        }
-
-        public byte[] ReadSlot(int box, int slot)
-        {
-            var bytes = com.ReadBytes(GetSlotOffset(box, slot), SlotSize + GapSize);
-            if (GapSize == 0)
-                return bytes;
-            var StoredLength = SlotSize - 0x1C;
-            var stored = bytes.Slice(0, StoredLength);
-            var party = bytes.Slice(StoredLength + 0x70, 0x1C);
-            return ArrayUtil.ConcatAll(stored, party);
-        }
-        public byte[] ReadOffset(uint offset) => com.ReadBytes(offset, SlotSize);
-
-        public void SendBox(byte[] boxData, int box)
-        {
-            byte[][] pkmData = boxData.Split(SlotSize);
-            for (int i = 0; i < SlotCount; i++)
-                SendSlot(pkmData[i], box, i);
-        }
-
-        public void SendSlot(byte[] data, int box, int slot)
-        {
-            var slotofs = GetSlotOffset(box, slot);
-            com.WriteBytes(data, slotofs);
-        }
+        private uint GetBoxOffset(int box) => (uint)BoxStart + (uint)(SlotSize * SlotCount * box);
+        private uint GetSlotOffset(int box, int slot) => GetBoxOffset(box) + (uint)(SlotSize * slot);
+        public byte[] ReadBox(int box, int len) =>  sys.ReadBytes(GetBoxOffset(box), len);
+        public byte[] ReadSlot(int box, int slot) => sys.ReadBytes(GetSlotOffset(box, slot), SlotSize);
     }
 }
