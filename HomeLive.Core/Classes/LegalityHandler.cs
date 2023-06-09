c9ed30b653231411194a136c0b02f035861b40a1
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-06-09 17:16:28 +0200

    Legality tweaks

--- HomeLive.Core/Classes/LegalityHandler.cs
+++ HomeLive.Core/Classes/LegalityHandler.cs
@@ -28,10 +28,10 @@ public static class LegalityHandler
 
     private static void FixCopyHeight(this PKM pkm)
     {
-        //Most PA8 conversions have the Copy Height (Scale) as 0. Try to fix by copying from the value from the actual Height.
+        //Most PH1 PA8 conversions have the Copy Height (Scale) as 0. Try to fix by copying from the value from the actual Height.
         if (pkm is PA8 pa8)
         {
-            //CopyHeight legality errors falls under the Encounter CheckIdentifier.
+            //CopyHeight legality errors falls under the "Encounter" CheckIdentifier.
             //There are more Encounter CheckResults, so iterate through all of them just to be sure.
             var legality = new LegalityAnalysis(pkm);
             var encs = legality.Results.Where(e => e.Identifier is CheckIdentifier.Encounter).ToArray();
@@ -45,8 +45,8 @@ public static class LegalityHandler
 
                     //Check if the legality has been fixed
                     var newencs = new LegalityAnalysis(clone).Results.Where(e => e.Identifier is CheckIdentifier.Encounter).ToArray();
-                    var oldInvalid = encs.Where(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid).Count();
-                    var newInvalid = newencs.Where(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid).Count();
+                    var oldInvalid = encs.Count(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid);
+                    var newInvalid = newencs.Count(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid);
                     var success = newInvalid < oldInvalid;
 
                     //If it's legal now, apply the CopyHeight to the actual PKM and stop the iteration
@@ -60,18 +60,49 @@ public static class LegalityHandler
         }
     }
 
+    private static void FixConversionSV(this PKM pkm)
+    {
+        var legality = new LegalityAnalysis(pkm);
+        if (!legality.Valid && pkm is PK8 pk8)
+        {
+            if (pk8.Version is (int)GameVersion.SL)
+            {
+                pk8.Version = (int)GameVersion.SW;
+                pk8.Met_Location = LocationsHOME.SWSL;
+            }
+            else if (pk8.Version == (int)GameVersion.VL)
+            {
+                pk8.Version = (int)GameVersion.SH;
+                pk8.Met_Location = LocationsHOME.SHVL;
+            }
+        }
+    }
+
     private static void FixConversionPLA(this PKM pkm)
     {
-        const int PLA_DEFLOC = 60000;
-
-        //PLA -> SWSH GameData conversion incorrectly set a wrong GameVersion, Met Location and Egg Met Location
         var legality = new LegalityAnalysis(pkm);
-        if (!legality.Valid && pkm is PK8 pk8 && (GameVersion)pk8.Version is GameVersion.PLA)
+        if (!legality.Valid && (GameVersion)pkm.Version is GameVersion.PLA)
         {
-            pk8.Version = (int)GameVersion.SW;
-            pk8.Met_Location = PLA_DEFLOC;
-            if (pk8.Egg_Location == 65534)
-                pk8.Egg_Location = 0;
+            //Set Ability number for PLA mons
+            if (legality.Results.Any(r => r.Identifier is CheckIdentifier.Ability && r.Judgement is Severity.Invalid))
+            {
+                var index = legality.EncounterMatch.Ability switch
+                {
+                    AbilityPermission.OnlyFirst or AbilityPermission.Any12 => 0,
+                    AbilityPermission.OnlySecond => 1,
+                    AbilityPermission.OnlyHidden or AbilityPermission.Any12H => 2,
+                    _ => 0,
+                };
+                pkm.SetAbilityIndex(index);
+            }
+            //PLA -> SWSH PH1 GameData conversion incorrectly set a wrong GameVersion, Met Location and Egg Met Location
+            if (pkm is PK8 pk8)
+            {
+                pk8.Version = (int)GameVersion.SW;
+                pk8.Met_Location = LocationsHOME.SWLA;
+                if (pk8.Egg_Location == 65534)
+                    pk8.Egg_Location = 0;
+            }
         }
     }
 
@@ -85,7 +116,7 @@ public static class LegalityHandler
                 //Only apply if Egg Location is 0 but recognized as Egg
                 if (pkm is PB8 pb8 && pb8.Egg_Location == 0)
                     pb8.Egg_Location = ushort.MaxValue;
-                //PK8 Origin Game incorrectly applied as BD or SP, should be applied as Sword or Shield
+                //BDSP origin PH1 -> PK8 Origin Game incorrectly applied as BD or SP, should be applied as Sword or Shield
                 else if (pkm is PK8 pk8)
                     pk8.FixSinnohToGalar();
             }
@@ -94,19 +125,16 @@ public static class LegalityHandler
 
     private static void FixSinnohToGalar(this PKM pkm)
     {
-        const int SP_DEFLOC = 59998;
-        const int BD_DEFLOC = 59999;
-
         //PK8s Can not have origin game as Brilliant Diamond or Shining Pearl
         if ((GameVersion)pkm.Version is GameVersion.BD)
         {
             pkm.Version = (int)GameVersion.SW;
-            pkm.Met_Location = BD_DEFLOC;
+            pkm.Met_Location = LocationsHOME.SWBD;
         }
         else if ((GameVersion)pkm.Version is GameVersion.SP)
         {
             pkm.Version = (int)GameVersion.SH;
-            pkm.Met_Location = SP_DEFLOC;
+            pkm.Met_Location = LocationsHOME.SHSP;
         }
 
         //All the PB8 are set as Egg during the conversion, check the encounter and eventually unset the Egg location
@@ -138,16 +166,20 @@ public static class LegalityHandler
                 if (AVs[i] > 0)
                     pb7.SetAV(i, AVs[i]);
 
-            //Recalc CP if there's a CP legality result
-            var invalidEncs = legality.Results.Where(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid).Count();
-            if(invalidEncs > 0)
+            //Reset ability
+            if (legality.Results.Any(r => r.Identifier is CheckIdentifier.Ability && r.Judgement is Severity.Invalid))
             {
-                var clone = pb7.Clone();
-                clone.ResetCP();
-                var newInvalid = new LegalityAnalysis(clone).Results.Where(r => r.Identifier is CheckIdentifier.Encounter && r.Judgement is Severity.Invalid).Count();
-                if (newInvalid < invalidEncs)
-                    pb7.ResetCP();
+                var index = legality.EncounterMatch.Ability switch
+                {
+                    AbilityPermission.OnlyFirst or AbilityPermission.Any12 => 0,
+                    AbilityPermission.OnlySecond => 1,
+                    AbilityPermission.OnlyHidden or AbilityPermission.Any12H => 2,
+                    _ => 0,
+                };
+                pkm.SetAbilityIndex(index);
             }
+
+            pb7.ResetCP();
         }
     }
     
