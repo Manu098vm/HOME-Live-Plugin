45364692a31320508093e22805c4498daae8c537
Manu <52102823+Manu098vm@users.noreply.github.com>
2024-06-11 03:18:15 +0200

    Bump version

--- HomeLive.Plugins/HomePlugin.cs
+++ HomeLive.Plugins/HomePlugin.cs
@@ -5,7 +5,7 @@ namespace HomeLive.Plugins;
 
 public class HomePlugin : IPlugin
 {
-    public const string Version = "3.2.3";
+    public const string Version = "3.2.4";
     public string Name => nameof(HomePlugin);
     public int Priority => 1;
 
